9e09f3b01b1b1ca8a9134f0d7f2e4b7f3bac6406
Vladimir <kent17@bk.ru>
2021-12-06 20:36:25 +0300

    make it better

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import statistics
 import sqlite3
 import os
 from flask import Flask
-bot = telebot.TeleBot("5058162485:AAHGx9-XieFGAaHLb3cVumTcokI1RkwGJbg")
+TOKEN = "5058162485:AAHGx9-XieFGAaHLb3cVumTcokI1RkwGJbg"
+bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 user_dict = {}
 class User:
@@ -187,10 +188,18 @@ def callback_worker(call):
 def handle_docs_audio(message):
     send_keyboard(message, text="Я не понимаю :-( Выбери один из пунктов меню:")
 
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://git.heroku.com/fit-o-bot.git' + "5058162485:AAHGx9-XieFGAaHLb3cVumTcokI1RkwGJbg")
+    bot.set_webhook(url='https://git.heroku.com/fit-o-bot.git' + TOKEN)
     return "!", 200
 
 if __name__ == "__main__":
