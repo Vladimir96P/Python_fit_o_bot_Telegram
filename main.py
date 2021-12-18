2246d0940240d8697fac21d7f0cfa4907d32664f
Vladimir <kent17@bk.ru>
2021-12-19 02:11:28 +0300

    make it better

--- main.py
+++ main.py
@@ -26,37 +26,23 @@ except:
 def send_welcome(message):
     msg = bot.reply_to(message, "Хэй! Меня зовут Фитобот😊 А тебя как? Напиши, пожалуйста, свое имя, возраст, рост в метрах и пол (русскоязычной раскладкой) на моем примере: Фитобот 25 1.75 М")
     bot.register_next_step_handler(msg, user_name)
-def send_keyboard(message, text = "Выбери интересующий раздел 😉"):
-    keyboard = types.ReplyKeyboardMarkup(row_width=2)
-    itembtn0 = types.KeyboardButton('Хочу сообщить свой текущий вес 😱')
-    itembtn1 = types.KeyboardButton('Хочу посмотреть статистику своего веса 😈')
-    itembtn2 = types.KeyboardButton('Хочу согнать жирок 🥦')
-    itembtn3 = types.KeyboardButton('Хочу поднабрать массы 💪')
-    itembtn4 = types.KeyboardButton('Удалить последнюю запись веса')
-    itembtn5 = types.KeyboardButton("Удалить все записи")
-    keyboard.add(itembtn0, itembtn1, itembtn2, itembtn3, itembtn4, itembtn5)
-    msg = bot.send_message(message.from_user.id, text=text, reply_markup=keyboard)
-    bot.register_next_step_handler(msg, callback_worker)
 def user_name(msg):
     print(msg)
     try:
-        print(msg.text)
         name = msg.text.split()[0].title()
-        print(name)
         age = msg.text.split()[1]
         age = int(age)
-        print(age)
         height = msg.text.split()[2]
         height = float(height)
         sex = msg.text.split()[3].title()
-        print(name, age, height, sex)
         if sex == 'М' or sex == 'Ж':
+            print('True')
             with sqlite3.connect('fit_o_bot.db') as con:
-                con.isolation_level = None
                 cursor = con.cursor()
                 cursor.execute('''
                        INSERT INTO bot_users_list (user_id, name, age, height, sex) 
-                       VALUES (?, ?);''', (msg.from_user.id, name, age, height, sex))
+                       VALUES (?, ?, ?, ?, ?);''', (msg.from_user.id, name, age, height, sex))
+                con.commit()
                 cursor.close()
             msg = bot.send_message(msg.from_user.id, f'''Привет! Приятно познакомиться, {name} 😜. Выбирай интересующую тебя тему:) И при первом использовании не забудь ввести свой текущий вес!)''')
             send_keyboard(msg)
