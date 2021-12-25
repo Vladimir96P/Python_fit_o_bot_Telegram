6805a23849b15997719a122a462ab9a9bcd39216
Vladimir <kent17@bk.ru>
2021-12-25 20:43:24 +0300

    make it better

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ try:
 except:
     pass
 DB_URL = os.environ["postgres://qmvydayqnuuxxz:40dc9792c9d15977ed989756198fbbba01983157173a98d5840e92c8c71928a8@ec2-54-74-102-48.eu-west-1.compute.amazonaws.com:5432/dcanatqglrancq"]
-
+db_con = psycopg2.connect(DB_URL, sslmode='require')
 @bot.message_handler(commands=['start'])
 
 def send_welcome(message):
@@ -73,11 +73,9 @@ def user_name(msg):
                        VALUES (?, ?, ?, ?, ?);''', (msg.from_user.id, name, age, height, sex))
                 con.commit()
                 cursor.close()
-            with psycopg2.connect(DB_URL, sslmode="require") as db_con:
                 db_obj = db_con.cursor()
                 db_obj.execute('''INSERT INTO bot_users_list (user_id, name, age, height, sex) VALUES (%s, %s, %s, %s, %s);''', (msg.from_user.id, name, age, height, sex))
                 db_con.commit()
-                db_obj.close()
             message = bot.send_message(msg.from_user.id, f'''Привет! Приятно познакомиться, {name} 😜. Выбирай интересующую тебя тему:) И при первом использовании не забудь ввести свой текущий вес!)''')
             send_keyboard(msg)
     except Exception as e:
